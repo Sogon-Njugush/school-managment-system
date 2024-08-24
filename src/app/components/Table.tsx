ae37207616a3ca2166c942b3c7cb7980e3deeb0c
Sogon-Njugush <sogon5045@gmail.com>
2024-08-24 13:48:25 +0300

    lists ui

--- src/app/components/Table.tsx
+++ src/app/components/Table.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Table = ({columns, renderRow, data}:{columns:{header:string; accessor:string, className?:string}[];
+const Table = ({columns, renderRow, data,}:{columns:{header:string; accessor:string; className?:string}[];
      renderRow: (item:any) => React.ReactNode; data:any[];}) => {
   return (
     <table className='w-full mt-4 '>
